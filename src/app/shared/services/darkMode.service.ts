fd939a13ba91765eb6b251cd4fe37cbd033bdddc
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-02-18 00:07:18 +0330

    [fix] fix dark mode logic

--- src/app/shared/services/darkMode.service.ts
+++ src/app/shared/services/darkMode.service.ts
@@ -6,7 +6,15 @@ import { Injectable } from '@angular/core';
 export class DarkModeService {
   darkMode: boolean = false;
 
+  constructor() {
+    if (localStorage.getItem('DarkTheme'))
+      this.darkMode =
+        localStorage.getItem('DarkTheme') == 'true' ? true : false;
+    else localStorage.setItem('DarkTheme', 'false');
+  }
+
   changeStatus() {
     this.darkMode = !this.darkMode;
+    localStorage.setItem('DarkTheme', this.darkMode ? 'true' : 'false');
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   prefix: "",
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{html,ts}"],
-  },
+  content: ["./src/**/*.{html,ts}"],
   darkMode: "class",
   theme: {
     extend: {},
